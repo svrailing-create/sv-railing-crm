98f08cb19aa92a4a6d93f1c2b16bf1a4dc1f4663
svrailing-create <svrailing@gmail.com>
2026-05-24 01:05:39 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Layout from './components/layout/Layout.jsx'
-import Dashboard from './pages/Dashboard.jsx'
+import App from './App.jsx'
 import './index.css'
 
-// Direct connection rendering our interface onto the page
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Layout>
-      <Dashboard />
-    </Layout>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 )
